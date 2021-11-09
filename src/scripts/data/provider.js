f46e7c3665b8c16684eee2ed4b355a585ece6ddb
LeahDaniel <leah.daniel1997@gmail.com>
2021-11-09 15:54:10 -0600

    main.js and provider.js

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,5 +1,5 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
+const API = "http://localhost:8088"
+const mainContainer = document.querySelector(".giffygram")
 
 
 const applicationState = {
@@ -8,5 +8,80 @@ const applicationState = {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
-    }
+    },
+    users: [],
+    messages: [],
+    posts: [],
+    favorites: [],
+    follows: []
 }
+
+//Return a copy of the applicationState
+export const getUsers = () => {
+    return database.users.map(user => ({...user}))
+}
+export const getMessages = () => {
+    return database.messages.map(message => ({...message}))
+}
+export const getPosts = () => {
+    return database.posts.map(post => ({...post}))
+}
+export const getFavorites = () => {
+    return database.favorites.map(favorite => ({...favorite}))
+}
+export const getFollows = () => {
+    return database.follows.map(follow => ({...follow}))
+}
+
+//Fetch the API
+export const fetchUsers = () => {
+    return fetch(`${API}/users`)
+    .then(response => response.json())
+    .then(
+        (usersAPI) => {
+            // Store the external state in application state
+            applicationState.users = usersAPI
+        }
+    )
+}
+export const fetchMessages = () => {
+    return fetch(`${API}/messages`)
+    .then(response => response.json())
+    .then(
+        (messagesAPI) => {
+            // Store the external state in application state
+            applicationState.messages = messagesAPI
+        }
+    )
+}
+export const fetchPosts = () => {
+    return fetch(`${API}/posts`)
+    .then(response => response.json())
+    .then(
+        (postsAPI) => {
+            // Store the external state in application state
+            applicationState.posts = postsAPI
+        }
+    )
+}
+export const fetchFavorites = () => {
+    return fetch(`${API}/favorites`)
+    .then(response => response.json())
+    .then(
+        (favoritesAPI) => {
+            // Store the external state in application state
+            applicationState.favorites = favoritesAPI
+        }
+    )
+}
+export const fetchFollows = () => {
+    return fetch(`${API}/follows`)
+    .then(response => response.json())
+    .then(
+        (followsAPI) => {
+            // Store the external state in application state
+            applicationState.follows = followsAPI
+        }
+    )
+}
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,32 @@
+import { LoginForm } from "./auth/Login.js"
+import { fetchFavorites, fetchFollows, fetchMessages, fetchPosts, fetchUsers } from "./data/provider.js"
 import { GiffyGram } from "./GiffyGram.js"
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
+    
+    fetchUsers()
+    .then(() => fetchMessages())
+    .then(() => fetchPosts())
+    .then(() => fetchFollows())
+    .then(() => fetchFavorites())
+    .then(() => 
+        {if (user) {
+            applicationElement.innerHTML = GiffyGram()
+        } else {
+            applicationElement.innerHTML = LoginForm()
+        }
+    })
 
-    if (user) {
-        applicationElement.innerHTML = GiffyGram()
-    } else {
-        applicationElement.innerHTML = LoginForm()
-    }
 }
+
+renderApp()
+
+document.addEventListener("stateChanged",
+    customEvent => {
+        console.log("State has changed. Re-rendering HTML...")
+        render()
+    }
+)
