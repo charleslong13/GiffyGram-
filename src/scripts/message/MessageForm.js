c1e2e2e775e29b4acb38541e54bd87eaeff4343b
sseals1 <email@example.com>
2021-11-10 13:11:31 -0600

    made changes to the html

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -8,28 +8,28 @@ export const MessageForm = () => {
     // created a variable html and assigned to it the value of the Direct Message form. On line 18 the users array is being itterated through to
     //get the userObjects and then on line 21 the userObj.name is being interpolated to render the Users name property for the message dropdown.
     let html = ` 
-            <h2>Direct Message</h2>
-
-            <div class="field">
-            <label class="label" for="recipient">Recipient:</label>
-            <select id="Recipient"</select>
-            <option value="0">--Choose a recipient...--</option>
-                
-            ${users.map(userObj => {
-                    return `
-                    ${userObj.name}
-
-                `
-            })}
+    <div class=“directMessage”>
+            <h3>Direct Message</h3>
+            <div>Recipient:
+                <select name=“directMessage__userSelect” class=“message__input”>
+                    <option value=“0">Choose a recipient...</option>
+                    ${users.map(userObj => {
+                        return `
+                        ${userObj.name}
+    
+                    `
+                })}
+                </select>
             </div>
-
-            <div class="field">
-                <label class="label" for="url">Message:</label>
-                <textarea name="message" value="Message ro user" class="input">Message to user<textarea/>
+            <div>
+                <label for=“message”>Message:</label>
+                <textarea name=“message” class=“message__input” type=“text” placeholder=“Message to user” </textarea>
             </div>
-
-            <button class="button" id="sendMessage">Send</button>
-            <button class="button" id="cancelMessage">Cancel</button>
+            <button id=“directMessage__submit”>Save</button>
+            <button id=“directMessage__cancel”>Cancel</button>
+            <button id=“directMessage__close”>x</button>
+        </div>
+            
         `
 
     return html
