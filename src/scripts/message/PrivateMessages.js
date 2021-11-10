e14ea54a0528b628e08bc84d146a33dea0c2d794
LeahDaniel <leah.daniel1997@gmail.com>
2021-11-10 14:26:58 -0600

    Notification Count

--- src/scripts/message/PrivateMessages.js
+++ src/scripts/message/PrivateMessages.js
@@ -10,3 +10,4 @@ export const PrivateMessages = () => {
         ${Footer()}
     `
 }
+
--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -1,3 +1,5 @@
+import {getMessages} from "../data/provider.js"
+
 export const NavBar = () => {
     return `        
     <nav class="navigation">
@@ -12,11 +14,22 @@ export const NavBar = () => {
             <img id="directMessageIcon" src="./images/fountain-pen.svg" alt="Direct message">
             <div class="notification__count">
                 ${/*replace with function to render dynamic message count*/''}
-                0
+                ${NotificationCount()}
             </div>
         </div>
         <div class="navigation__item navigation__logout">
             <button id="logout" class="fakeLink">Logout</button>
         </div>
     </nav>`
+}
+
+const NotificationCount = () => {
+    const messages = getMessages()
+
+    //create an array of message objects that do not have the "read" key, and thus are unread.
+    const unreadArray = messages.filter(message => !message.read)
+
+    //return a string of the length of the array (the number of unread objects)
+    return `${unreadArray.length}`
+
 }
