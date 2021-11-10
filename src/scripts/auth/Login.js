96e5c661bd23725fcf79719a661e1d89a4333f88
charleslong13 <charleslong13@icloud.com>
2021-11-10 11:39:05 -0600

    added comments for login code

--- src/scripts/auth/Login.js
+++ src/scripts/auth/Login.js
@@ -1,20 +1,20 @@
 import { getUsers } from "../data/provider.js"
 
-
+//click event listener to listen for a click on the loginButton
 document.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "loginButton") {
         let foundUser = null
         const userState = getUsers()
-
+//get the user input and store its value in a variable 
         const email = document.querySelector("input[name='email']").value
         const password = document.querySelector("input[name='password']").value
-
+//iterate through users array and if the user inputted email and password are true then reassign foundUser to its value 
         for (const user of userState) {
             if (user.email === email && user.password === password) {
                 foundUser = user
             }
         }
-
+//if found user is not null (i.e. if it exists) set the found user's id in local storage 
         if (foundUser !== null) {
             localStorage.setItem("gg_user", foundUser.id)
             document.querySelector(".giffygram").dispatchEvent(new CustomEvent("stateChanged"))
