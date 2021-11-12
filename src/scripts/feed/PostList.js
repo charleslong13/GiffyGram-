fab707d05bd4d7993c57e51325e7bf3ff61c3e59
charleslong13 <charleslong13@icloud.com>
2021-11-12 09:40:27 -0600

    when clicked the star changes

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -26,12 +26,11 @@ export const PostList = () => {
                 </a>
                 on ${postObj.datePosted}
             </div>
-            <div class="post__actions">
+            <div id="post__action" class="post__actions">
                 <div>
                     ${/*These are both of the star options. Logic will need to be added to determine
                     which one should be shown */''}
-                    <img id="favoritePost--" class="actionIcon" src="./images/favorite-star-blank.svg">
-                    <img id="favoritePost--" class="actionIcon" src="./images/favorite-star-yellow.svg">
+                    ${renderFavoriteButton()}
                 </div>
                 <div>
                     <img id="blockPost--" class="actionIcon" src="./images/block.svg">
@@ -42,3 +41,36 @@ export const PostList = () => {
     </section>`
     
 }
+
+
+const renderFavoriteButton = () => {
+    return `<img id="favoritePost--" class="actionIcon" src="./images/favorite-star-blank.svg"></img>`
+}
+
+const renderSelectedFavoriteButton = () => {
+    return `<img id="favoritePost-" class="actionIcon" src="./images/favorite-star-yellow.svg"></img>`
+}
+
+//this event listener is responsible for changing the empty star to a filled in star when clicked and vice versa 
+document.addEventListener(
+    "click",
+    (event) => {
+        if (event.target.id === "favoritePost--") {
+            //created a new div and gave it a class of postFormContainer in our giffygram module, below we are directing where our post form renders 
+            const Element = document.getElementById("post__action")
+            //the line below is responsible for rendering the form as html
+            Element.innerHTML = renderSelectedFavoriteButton()
+        } else if (event.target.id === "favoritePost-") {
+            Element = document.getElementById("post__action")
+            Element.innerHTML = renderFavoriteButton()
+        }
+    }
+)
+
+document.addEventListener(
+    "click",
+    (event) => {
+        if (event.target.id === "favoritePost--") {
+
+{/* <img id="favoritePost--" class="actionIcon" src="./images/favorite-star-blank.svg">
+                    <img id="favoritePost--" class="actionIcon" src="./images/favorite-star-yellow.svg"></img> */}
