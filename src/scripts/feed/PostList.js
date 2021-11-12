f5bd5a8e297e311b40e90871c3d30636caadb475
charleslong13 <charleslong13@icloud.com>
2021-11-12 13:04:22 -0600

    favorite button functional

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,4 +1,4 @@
-import { getPosts, getUsers, DeletePost, getCurrentUser } from '../data/provider.js'
+import { getPosts, getUsers, DeletePost, getCurrentUser, getUserFavoritePosts, DeleteFavorite, getFavorites, saveNewFavorite } from '../data/provider.js'
 
 
 export const PostList = () => {
@@ -30,7 +30,8 @@ export const PostList = () => {
                 <div>
                     ${/*These are both of the star options. Logic will need to be added to determine
                     which one should be shown */''}
-                    <img id="favoritePost--" class="actionIcon" src="./images/favorite-star-blank.svg"></img>
+                    <img id="favoritePost--" name="star--${postObj.id}" class="actionIcon" src="./images/favorite-star-blank.svg"></img>
+                    <img id="favoritePost-" name="star--${postObj.id}" class="actionIcon" src="./images/favorite-star-yellow.svg"></img>
                 </div>
                 
                 
@@ -49,77 +50,34 @@ export const PostList = () => {
     </section>`
 
 }
-export const PostList2 = () => {
-    const posts = getPosts()
-    const users = getUsers()
-
-    return `<section class="posts">
-    ${posts.map(postObj => {
-        const foundUser = users.find(userObj => {
-            return postObj.userId === userObj.id
-        })
-
-        return `
-        <div class="post">
-            <header>
-                <h2 class="post__title">${postObj.title}</h2>
-            </header>
-            <img class="post__image" src="${postObj.imageURL}">
-            <div class="post__description">
-                ${postObj.description}
-            </div>
-            <div class="post__tagline">
-                <a href="#" class="profileLink" id="profile--${foundUser.id}">
-                    Posted by: ${foundUser.name}
-                </a>
-                on ${postObj.datePosted}
-            </div>
-            <div id="post__action" class="post__actions">
-                <div>
-                    ${/*These are both of the star options. Logic will need to be added to determine
-                    which one should be shown */''}
-                    <img id="favoritePost-" class="actionIcon" src="./images/favorite-star-yellow.svg"></img>
-                </div>
-                
-                
-                ${postObj.userId === getCurrentUser().currentUserId // if statement that checks if the userId on a post is the same as the  logged in user. If true the then generate the html which generates the trashcan Icon
-                ? ` <div> 
-                        <img id="blockPost--${postObj.id}" class="actionIcon" src="./images/block.svg">
-                    </div>`
-                : ""
-            }
-                
-                
-                
-            </div>
-        </div>`
-    }).join("")}
-    </section>`
-
-}
-
-
-
-//this event listener is responsible for changing the empty star to a filled in star when clicked and vice versa 
+//this event listener is responsible for checking to see if a post has been favorited. If it has not then it will post the favorited post data to the api
+// checks if the post has already been favorited and if it has then it will remove the favorited post object from the favorites array 
 document.addEventListener(
     "click",
     (event) => {
-        if (event.target.id === "favoritePost--") {
-            //created a new div and gave it a class of postFormContainer in our giffygram module, below we are directing where our post form renders 
-            const Element = document.getElementById("postFeed")
-            //the line below is responsible for rendering the form as html
-            Element.innerHTML = PostList2()
-        } else if (event.target.id === "favoritePost-") {
-            Element = document.getElementById("postFeed")
-            Element.innerHTML = PostList()
-        }
-    }
-)
+        if (event.target.name.startsWith("star")) {
+                const [, postObjString] = event.target.name.split("--") 
+                const postObjId = parseInt(postObjString)
+                const userFavoritedPosts = getUserFavoritePosts()
+                const favorites = getFavorites()
+                const currentUserId = getCurrentUser().currentUserId
+                //finding an object in user favorited posts where the postObj.id is equal to the clicked star's post object id and storing it in a variable
+                const foundFavoritedPost = userFavoritedPosts.find(postObj => postObj.id === postObjId)
+                //if the foundFavoritedPost is truthy find the matching favorite and delete it from our favorites array 
+                if(foundFavoritedPost) {
+                    const foundFavorite = favorites.find(favorite => foundFavoritedPost.id === favorite.postId && favorite.userId === currentUserId)
+                    DeleteFavorite(foundFavorite.id)
+                // if the foundFavoritedPost is falsey then post the newFavoriteObject to the api/favorites array 
+                } else {
+                    const newFavoriteObject = {
+                        postId: postObjId,
+                        userId: currentUserId
+                    } 
+                    saveNewFavorite(newFavoriteObject)
+                }
+        }})
 
-document.addEventListener(
-    "click",
-    (event) => {
-        if (event.target.id === "favoritePost--") {
+
 
             
             //add click event. target is = to  . startswith "blockPost-- .split  pull postObj.id
@@ -131,8 +89,7 @@ document.addEventListener(
                     }
                 })
 
-        }
-    })
+  
 
 
 
